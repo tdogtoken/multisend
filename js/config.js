e813a61b8d447be81b64b9fd94b78b8b927fbfff
tdogtoken <105568891+tdogtoken@users.noreply.github.com>
2022-06-21 16:03:50 -0400

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,16 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 1.change this address to your own address
+// 2.set how much fee you will recieve,for example:0.01 
+// 3.client total fee= contract small fee + your setting fee
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var FeeReciever = "0x4DF913829ECad656923800774Fa944c0C7981d7C";
+
+
+var BinanceFee=0.01;
+var PolygonFee=0.1;
+var FantomFee=0.12;
+var RopstenFee=0.00001;
+var HuobiFee=0.01;
+var AvalancheFee=0.001;
+var EthereumFee=0.0001;
